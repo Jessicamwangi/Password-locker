7bc56184d95a594b6e20787e0aaa4ea6718f2a01
Jessica <jessicamwangi933@gmail.com>
2022-02-21 09:56:56 +0300

    created delete credential and find cretential functions and added a tear down function

--- password_test.py
+++ password_test.py
@@ -53,3 +53,40 @@ class TestCredentials(unittest.TestCase):
         """
         self.new_credential.save_details()
         self.assertEqual(len(Credentials.credentials_list),1)
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
+
+    def test_save_many_accounts(self):
+        '''
+        test to check if we can save multiple credentials objects to our credentials list
+        '''
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter","jessicamwangi","tess7A") 
+        test_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_delete_credential(self):
+        """
+        test method to test if we can remove an account credentials from our credentials_list
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter", "jessicamwangi","tess7A")
+        test_credential.save_details()
+
+        self.new_credential.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_credential(self):
+        """
+        test to check if we can find a credential entry by account name and display the details of the credential
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter","jessicamwangi","tess7A") 
+        test_credential.save_details()
+
+        the_credential = Credentials.find_credential("Twitter")
+
+        self.assertEqual(the_credential.account,test_credential.account)
