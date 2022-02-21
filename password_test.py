f714f12525c766d2c25fdf5ae6a6c4c228659125
Jessica <jessicamwangi933@gmail.com>
2022-02-21 09:50:49 +0300

    created a test credential class and added credential functions

--- password_test.py
+++ password_test.py
@@ -26,3 +26,30 @@ class TestClass(unittest.TestCase):
         """
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+
+        """
+        self.new_credential = Credentials('Gmail','Jessica','Deks2e9')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Gmail')
+        self.assertEqual(self.new_credential.userName,'Jessica')
+        self.assertEqual(self.new_credential.password,'Deks2e9')
+
+    def save_credential_test(self):
+        """
+        test case to test if the crential object is saved into the credentials list.
+
+        """
+        self.new_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),1)
